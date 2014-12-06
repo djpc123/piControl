330cdab25f819dbb82267dbc8cc6e74fdf84518a
Chris Ewbank <ce90@kent.ac.uk>
2014-12-06 13:45:26 +0000

    Add main

--- controller/controller.py
+++ controller/controller.py
@@ -2,14 +2,39 @@
 import time
 import RPi.GPIO as GPIO
 
+PIN_1 = 11
+PIN_2 = 15
+PIN_3 = 16
+PIN_4 = 13
 
-SWITCH_1 = 11
-SWITCH_2 = 15
-SWITCH_3 = 16
-SWITCH_4 = 13
+ON_OFF_KEY = 18
+ENABLE = 22
 
-ON = ['1011', '1111', '1110', '1101', '1100']
-OFF = ['0011', '0111', '0110', '0101', '0100']
+ON = [
+    # All
+    [True, False, True, True],
+    # 1
+    [True, True, True, True],
+    # 2
+    [True, True, True, False],
+    # 3
+    [True, True, False, True],
+    #4
+    [True, True, False, False]
+]
+
+OFF = [
+    # All
+    [False, False, True, True],
+    # 1
+    [False, True, True, True],
+    # 2
+    [False, True, True, False],
+    # 3
+    [False, True, False, True],
+    # 4
+    [False, True, False, False]
+]
 
 
 class SwitchController(object):
@@ -18,149 +43,72 @@ class SwitchController(object):
         GPIO.setmode(GPIO.BOARD)
 
         # Select the GPIO pins used for the encoder K0-K3 data inputs
-        GPIO.setup(SWITCH_1, GPIO.OUT)
-        GPIO.setup(SWITCH_2, GPIO.OUT)
-        GPIO.setup(SWITCH_3, GPIO.OUT)
-        GPIO.setup(SWITCH_4, GPIO.OUT)
+        GPIO.setup(PIN_1, GPIO.OUT)
+        GPIO.setup(PIN_2, GPIO.OUT)
+        GPIO.setup(PIN_3, GPIO.OUT)
+        GPIO.setup(PIN_4, GPIO.OUT)
 
         # Select the signal to select ASK/FSK
-        GPIO.setup(18, GPIO.OUT)
+        GPIO.setup(ON_OFF_KEY, GPIO.OUT)
 
         # Select the signal used to enable/disable the modulator
-        GPIO.setup(22, GPIO.OUT)
+        GPIO.setup(ENABLE, GPIO.OUT)
 
         # Disable the modulator by setting CE pin lo
-        GPIO.output(22, False)
+        GPIO.output(ENABLE, False)
 
         # Set the modulator to ASK for On Off Keying
         # by setting MODSEL pin lo
-        GPIO.output(18, False)
+        GPIO.output(ON_OFF_KEY, False)
 
         # Initialise K0-K3 inputs of the encoder to 0000
-        GPIO.output(11, False)
-        GPIO.output(15, False)
-        GPIO.output(16, False)
-        GPIO.output(13, False)
+        GPIO.output(PIN_1, False)
+        GPIO.output(PIN_2, False)
+        GPIO.output(PIN_3, False)
+        GPIO.output(PIN_4, False)
 
-        try:
-            # We will just loop round switching the units on and off
-            while True:
-                raw_input('hit return key to send socket 1 ON code')
-                # Set K0-K3
-                print "sending code 1111 socket 1 on"
-                GPIO.output(11, True)
-                GPIO.output(15, True)
-                GPIO.output(16, True)
-                GPIO.output(13, True)
-                # let it settle, encoder requires this
-                time.sleep(0.1)
-                # Enable the modulator
-                GPIO.output(22, True)
-                # keep enabled for a period
-                time.sleep(0.25)
-                # Disable the modulator
-                GPIO.output(22, False)
+    def change_plug_state(self, pin_states):
+        GPIO.output(PIN_1, pin_states[1])
+        GPIO.output(PIN_2, pin_states[2])
+        GPIO.output(PIN_3, pin_states[3])
+        GPIO.output(PIN_4, pin_states[4])
+        time.sleep(0.1)
+        GPIO.output(ENABLE, True)
+        time.sleep(0.25)
+        GPIO.output(ENABLE, False)
 
-                raw_input('hit return key to send socket 1 OFF code')
-                # Set K0-K3
-                print "sending code 0111 Socket 1 off"
-                GPIO.output(11, True)
-                GPIO.output(15, True)
-                GPIO.output(16, True)
-                GPIO.output(13, False)
-                # let it settle, encoder requires this
-                time.sleep(0.1)
-                # Enable the modulator
-                GPIO.output(22, True)
-                # keep enabled for a period
-                time.sleep(0.25)
-                # Disable the modulator
-                GPIO.output(22, False)
+    def switch_on(self, socket):
+        self.change_plug_state(ON[socket])
 
-                raw_input('hit return key to send socket 2 ON code')
-                # Set K0-K3
-                print "sending code 1110 socket 2 on"
-                GPIO.output(11, False)
-                GPIO.output(15, True)
-                GPIO.output(16, True)
-                GPIO.output(13, True)
-                # let it settle, encoder requires this
-                time.sleep(0.1)
-                # Enable the modulator
-                GPIO.output(22, True)
-                # keep enabled for a period
-                time.sleep(0.25)
-                # Disable the modulator
-                GPIO.output(22, False)
+    def switch_off(self, socket):
+        self.change_plug_state(OFF[socket])
 
-                raw_input('hit return key to send socket 2 OFF code')
-                # Set K0-K3
-                print "sending code 0110 socket 2 off"
-                GPIO.output(11, False)
-                GPIO.output(15, True)
-                GPIO.output(16, True)
-                GPIO.output(13, False)
-                # let it settle, encoder requires this
-                time.sleep(0.1)
-                # Enable the modulator
-                GPIO.output(22, True)
-                # keep enabled for a period
-                time.sleep(0.25)
-                # Disable the modulator
-                GPIO.output(22, False)
+    def wait_for_input(self):
+        # We will just loop round switching the units on and off
+        while True:
+            raw_input('hit return key to send socket 1 ON code')
+            self.switch_on(1)
 
-                raw_input('hit return key to send ALL ON code')
-                # Set K0-K3
-                print "sending code 1011 ALL on"
-                GPIO.output(11, True)
-                GPIO.output(15, True)
-                GPIO.output(16, False)
-                GPIO.output(13, True)
-                # let it settle, encoder requires this
-                time.sleep(0.1)
-                # Enable the modulator
-                GPIO.output(22, True)
-                # keep enabled for a period
-                time.sleep(0.25)
-                # Disable the modulator
-                GPIO.output(22, False)
+            raw_input('hit return key to send socket 1 OFF code')
+            self.switch_off(1)
 
-                raw_input('hit return key to send ALL OFF code')
-                # Set K0-K3
-                print "sending code 0011 All off"
-                GPIO.output(11, True)
-                GPIO.output(15, True)
-                GPIO.output(16, False)
-                GPIO.output(13, False)
-                # let it settle, encoder requires this
-                time.sleep(0.1)
-                # Enable the modulator
-                GPIO.output(22, True)
-                # keep enabled for a period
-                time.sleep(0.25)
-                # Disable the modulator
-                GPIO.output(22, False)
+            raw_input('hit return key to send socket 2 ON code')
+            self.switch_on(2)
 
+            raw_input('hit return key to send socket 2 OFF code')
+            self.switch_off(2)
+
+            raw_input('hit return key to send ALL ON code')
+            self.switch_on(0)
+
+            raw_input('hit return key to send ALL OFF code')
+            self.switch_off(0)
+
+
+if __name__ == '__main__':
+    try:
+        controller = SwitchController()
+        controller.wait_for_input()
+    except KeyboardInterrupt:
         # Clean up the GPIOs for next time
-        except KeyboardInterrupt:
-            GPIO.cleanup()
-
-        def change_plug_state(socket, on_or_off):
-            state = on_or_off[socket][3] == '1'
-            GPIO.output(SWITCH_1, state)
-            state = on_or_off[socket][2] == '1'
-            GPIO.output(SWITCH_2, state)
-            state = on_or_off[socket][1] == '1'
-            GPIO.output(SWITCH_3, state)
-            state = on_or_off[socket][0] == '1'
-            GPIO.output(SWITCH_4, state)
-            sleep(0.1)
-            GPIO.output(ENABLE, True)
-            sleep(0.25)
-            GPIO.output(ENABLE, False)
-
-        def switch_on(socket):
-            change_plug_state(socket, ON)
-
-        def switch_off(socket):
-            change_plug_state(socket, OFF)
+        GPIO.cleanup()
